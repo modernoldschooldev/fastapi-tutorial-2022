9e767216251440f4356e4d6bd03bc041a891a1a8
John Ratliff <john@technoplaza.net>
2022-04-02 10:08:39 -0900

    add path and query params

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+
+from . import crud
 
 app = FastAPI()
 
@@ -6,3 +8,18 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "Hello, World"}
+
+
+@app.get("/movie/{id}", summary="Get single movie from database by ID")
+def get_movie(id: int = Path(..., description="Movie ID to retrive")):
+    return crud.get_movie_by_id(id)
+
+
+@app.get("/movies", summary="Retrieve list of movies from database")
+def get_movies(
+    limit: int = Query(
+        5, description="Limit of how many results to return", gt=0, lt=11
+    ),
+    offset: int = Query(0, description="Offset to start fetching movies from", gt=-1),
+):
+    return crud.get_movies(limit=limit, offset=offset)
